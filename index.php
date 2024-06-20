61c259db4aba88559e77bec65d45531bc4ba10ce
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-20 14:49:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dischi JSON</title>
+  <link rel="stylesheet" href="./css/styles.css">
+  <!-- VUE -->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <!-- AXIOS -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+  <div id="app"></div>
+  <!-- JS -->
+  <script src="./js/main.js"></script>
+</body>
+
+</html>
